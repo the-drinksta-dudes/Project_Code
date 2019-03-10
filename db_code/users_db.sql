32766dd52e8a7807b17a8d86ddd0eb9059ac66a1
Holden Kjerland-Nicoletti <45382054+holdenKjerland-Nicoletti@users.noreply.github.com>
2019-03-10 16:17:20 -0600

    Update users_db.sql

--- db_code/users_db.sql
+++ db_code/users_db.sql
@@ -1,5 +1,6 @@
 CREATE TABLE users(
    user_id SERIAL PRIMARY KEY,
+   name VARCHAR(100),
    username VARCHAR(40),
    password VARCHAR(255),
    email VARCHAR(255)
@@ -7,9 +8,9 @@ CREATE TABLE users(
 
 
 INSERT INTO users(username, password, email)
-VALUES('holden.nicoletti', 'password', 'hokj6625@colorado.edu')
-('trevor.stanley', 'password', 'trst9490@colorado.edu'),
-('andrew.yee', 'password', 'anye2688@colorado.edu'),
-('teagan.peters', 'password', 'tepe5782@colorado.edu'),
-('joshua.hamel', 'password', 'joha2422@colorado.edu'),
-('maxwell.sechelski', 'password', 'mase8617@colorado.edu');
+VALUES('Holden Nicoletti', 'holden.nicoletti', 'password', 'hokj6625@colorado.edu')
+('Trevor Stanley', 'trevor.stanley', 'password', 'trst9490@colorado.edu'),
+('Andrew Yee'. 'andrew.yee', 'password', 'anye2688@colorado.edu'),
+('Teagan Peters', 'teagan.peters', 'password', 'tepe5782@colorado.edu'),
+('Joshua Hamel', 'joshua.hamel', 'password', 'joha2422@colorado.edu'),
+('Maxwell Sechelski', 'maxwell.sechelski', 'password', 'mase8617@colorado.edu');
