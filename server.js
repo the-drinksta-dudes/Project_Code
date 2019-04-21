82d5b16a51c8e57feaf72d84c2896a6376770531
hameldacamel <joha2422@colorado.edu>
2019-04-21 14:07:04 -0600

    fixing get_Drink issue

--- server.js
+++ server.js
@@ -271,6 +271,7 @@ app.get('/home/get_drink', function(req, res)
   db.any(drink_search)
     .then(data => {
     	res.render('search', {
+    			req.params: {data[0].name}
 				my_title: "Drink Search",
 				drink: data[0],
 				username: ''
