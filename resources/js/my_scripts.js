0b3b450e712d7d4a168d4c86695f00a036af7b9d
hameldacamel <joha2422@colorado.edu>
2019-04-20 19:02:32 -0600

    committing invisible change

--- resources/js/my_scripts.js
+++ resources/js/my_scripts.js
@@ -1,20 +1,20 @@
 /*
-	players is an array to hold each player's information.
-	Fields:
-		name - Football player's name
-		img  - The relative/absolute path to the image file.
-		alt  - The alternative text that describes the image.
-		year - The student's year in college (Freshman, Sophomore, Junior, Senior).
-		major- The student's current college major.
-		games_played    - The number of football games the student has played for the Buffs.
-		pass_yards      - The total number of passing yards in the student's football career for the Buffs.
-		rushing_yards   - The total number of rushing yards in the student's football career for the Buffs.
-		receiving_yards - The total number of receiving yards in the student's football career for the Buffs.
+	Function: viewSearch
+	Used to make search options visible on the home page
 */
-var players = [{name:"John Doe", img: "../resources/img/player1.jpg", alt:"Image of Player 1", year:"Sophomore", major:"Art", games_played: 23, pass_yards: 435, rushing_yards: 200, receiving_yards: 88},
-				{name:"James Smith", img: "../resources/img/player2.jpg", alt:"Image of Player 2", year:"Junior", major:"Science", games_played: 17, pass_yards: 192, rushing_yards: 102, receiving_yards: 344},
-				{name:"Samuel Phillips", img: "../resources/img/player3.jpg", alt:"Image of Player 3", year:"Freshman", major:"Math", games_played: 8, pass_yards: 35, rushing_yards: 70, receiving_yards: 98},
-				{name:"Robert Myers", img: "../resources/img/player4.jpg", alt:"Image of Player 4", year:"Senior", major:"Computer Science", games_played: 31, pass_yards: 802, rushing_yards: 375, receiving_yards: 128}];
+function viewSearch(id, toggle)
+{
+if (toggle==1)
+	{
+		document.getElementById(id).style.height = "auto";
+		document.getElementById(id).style.visibility = "visible";
+	}
+else if(toggle==0)
+	{
+		document.getElementById(id).style.height = "0px";
+		document.getElementById(id).style.visibility = "hidden";
+	}	
+}
 
 
 /*
