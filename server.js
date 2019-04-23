fe13485a0bdd6e81feb9c253ecc4290838aec2f4
hameldacamel <joha2422@colorado.edu>
2019-04-22 18:11:46 -0600

    fixing

--- server.js
+++ server.js
@@ -367,7 +367,7 @@ app.post('/account/remove', function(req,res){
 app.get('/home/get_ingredient', function(req, res)
 {
 	var ingredient_name = req.query.ingredientname;
-	var ingredient_search = "select name from drinks where '"+ ingredient_name+"' = ANY(ingredients);";
+	var ingredient_search = "select name from drinks where '"+ ingredient_name+"' ILIKE ANY(ingredients);";
 	console.log(ingredient_name);
 
   db.any(ingredient_search)
@@ -391,43 +391,7 @@ app.get('/home/get_ingredient', function(req, res)
 
 });
 
-app.get('/search/get_drink01', function(req, res)
-{
-	var drink_name = req.query.drinkButton01;
-	var drink_search = "select * from drinks where name = '"+ drink_name + "';";
-	console.log(drink_search);
 
-  db.any(drink_search)
-    .then(data => {
-			var u_name = '';
-			var drink = data[0];
-
-			if(req.cookies.username){
-				u_name = req.cookies.username;
-			}
-
-    	res.render('search', {
-				my_title: "Drink Search",
-				drink: data[0],
-				username: u_name
-			})
-
-			window.localStorage.setItem("my_title", "Drink Search");
-			window.localStorage.setItem("drink", drink);
-			window.localStorage.setItem("username", u_name);
-    })
-    .catch(error => {
-		// display error message in case an error
-		console.log(error);
-            request.flash('error', err);
-            response.render('search', {
-								my_title: 'Drink Search',
-                drink: '',
-                username: ''
-            })
-    });
-
-});
 //app.listen(3000);
 //console.log('3000 is the magic port');
 app.listen(process.env.PORT);
